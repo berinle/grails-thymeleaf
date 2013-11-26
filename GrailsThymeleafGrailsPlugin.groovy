464232eb6d95fd741ddd3e8d633bfb13080fc7b9
Bayo Erinle <berinle@gmail.com>
2013-11-26 05:57:32 -0500

    skeleton shell for grails thymeleaf plugin

---
+++ GrailsThymeleafGrailsPlugin.groovy
@@ -0,0 +1,69 @@
+class GrailsThymeleafGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.3 > *"
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def title = "Grails Thymeleaf Plugin" // Headline display name of the plugin
+    def author = "Bayo Erinle"
+    def authorEmail = "berinle@gmail.com"
+    def description = '''\
+The Grails Thymeleaf plugin provides support for rendering Thymeleaf templates as views. 
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/grails-thymeleaf"
+
+    // Extra (optional) plugin metadata
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+//    def license = "APACHE"
+
+    // Details of company behind the plugin (if there is one)
+//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+    def issueManagement = [ system: "JIRA", url: "https://github.com/berinle/grails-thymeleaf/issues" ]
+
+    // Online location of the plugin's browseable source code.
+    def scm = [ url: "https://github.com/berinle/grails-thymeleaf" ]
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { ctx ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
+}
